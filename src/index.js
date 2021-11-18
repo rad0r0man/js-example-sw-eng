9a60d625fc37fef32dd276c9e29ea7f9c89714d3
rad0r0man <radoeom19@gmail.com>
2021-11-18 12:03:54 +0100

    simple UI created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import { Calculator } from './calculator/calculator'
 
-alert(new Calculator().add(1,1));
+const calcArea = document.getElementById('calcArea');
+const input1 = document.createElement('input');
+const input2 = document.createElement('input');
+
+const sumButton = document.createElement('button');
+sumButton.innerHTML = "Add";
+sumButton.addEventListener('click', () => alert(calculator.add(input1.value, input2.value)));
+
+calcArea.appendChild(input1);
+calcArea.appendChild(input2);
+calcArea.appendChild(sumButton);
